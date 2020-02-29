7c973c0f09b4316f963bcde2e2cc034aa6d6a15e
Semyon Evstigneev <semyon-evstigneev@yandex.ru>
2020-02-29 17:49:10 +0300

    add table without styles to study process

--- src/screens/studyProcess/index.vue
+++ src/screens/studyProcess/index.vue
@@ -16,8 +16,140 @@
     </nb-header>
 
     <nb-content padder>
-      <nb-text>Content goes here</nb-text>
+      <scroll-view :content-container-style="{contentContainer: {paddingVertical: 20}}">
+        <view class="header-content">
+          <view
+              v-for="(item, index) in headerData"
+              :key="index"
+              class="header-data-viewer">
+            <text class="header-data-viewer-label">{{item.label}}</text>
+            <text class="header-data-viewer-value">{{item.value}}</text>
+          </view>
+        </view>
+        <view class="student-status-container">
+          <text class="student-status-container-label">Статус:</text>
+          <text class="student-status-container-value">{{studentStatus}}</text>
+        </view>
+        <view>
+          <table :dataToShow="statusTableData"></table>
+        </view>
+        <view>
+          <table :dataToShow="NirSection"></table>
+        </view>
+      </scroll-view>
     </nb-content>
 
   </nb-container>
 </template>
+<script>
+import table from './table';
+
+import { bus } from "../../event bus";
+
+export default {
+  components: {
+    table,
+  },
+  name: 'StudyProcess',
+  computed: {
+    headerData() {
+      return [{
+        label: 'Средний балл',
+        value: '4.86',
+      }, {
+        label: 'Рейтинг по курсу',
+        value: '139',
+      }, {
+        label: 'Рейтинг по ФШ',
+        value: '123',
+      }]
+    },
+    studentStatus() {
+      return 'Учится';
+    },
+    statusTableData() {
+      return [{
+        label: 'Уровень образования',
+        value: '',
+        link: '',
+      }, {
+        label: 'Курс',
+        value: '1',
+        link: '',
+      }, {
+        label: 'Подразделение',
+        value: 'Физтех-школа Прикладной Математики и Информатики (ФПМИ)',
+        link: '',
+      }, {
+        label: 'Группа',
+        value: 'М05-907б',
+        link: '',
+      }, {
+        label: 'Направление',
+        value: '03.04.01 Прикладные математика и физика (магистратура)',
+        link: '',
+      }, {
+        label: 'Программа',
+        value: 'Системные исследования',
+        link: '',
+      }, {
+        label: 'Форма оплаты обучения',
+        value: 'Места за счет федерального бюджета',
+        link: '',
+      }];
+    },
+    NirSection() {
+      return [{
+        label: 'Базовая кафедра',
+        value: 'кафедра системных исследований',
+        link: '',
+      }, {
+        label: 'Научный руководитель',
+        value: 'Попков Юрий Соломонович',
+        link: '',
+      }, {
+        label: 'Тема дипломной работы',
+        value: '',
+        link: '',
+      }];
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+
+  .header-content {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .header-data-viewer {
+    display: flex;
+    width: 33%;
+  }
+
+  .header-data-viewer-label {
+    font-weight: bold;
+  }
+
+  .header-data-viewer-value {
+
+  }
+
+  .student-status-container {
+
+  }
+
+  .student-status-container-label {
+
+  }
+
+  .student-status-container-value {
+    border-color: green;
+    padding: 10px 25px;
+  }
+
+</style>
---
+++ src/screens/studyProcess/table.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<view
+				class="table-row"
+				:key="index"
+				v-for="(item, index) in dataToShow">
+			<text>
+				{{item.label}}
+			</text>
+			<text>
+				{{item.value}}
+			</text>
+		</view>
+	</view>
+</template>
+
+<script>
+
+export default {
+  name: 'table',
+  components: {},
+  props: {
+    dataToShow: Array,
+	},
+  computed: {},
+  methods: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+
+	.table-row {
+		border-width: 1px;
+		border-style: solid;
+		border-color: gray;
+	}
+</style>
