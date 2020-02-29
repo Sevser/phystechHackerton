fb2e226b7d70cbbb58e3bec5afa517852f73497b
Semyon Evstigneev <semyon-evstigneev@yandex.ru>
2020-02-29 19:49:58 +0300

    tempcommit

--- src/screens/schedule/index.vue
+++ src/screens/schedule/index.vue
@@ -15,16 +15,19 @@
       <nb-right />
     </nb-header>
 
-    <nb-content padder>
-      <scroll-view
-          v-if="scheduleReady"
-          :content-container-style="{contentContainer: {paddingVertical: 20}}">
-      </scroll-view>
-    </nb-content>
+    <nb-tabs :renderTabBar="getScollableTabComp">
+      <nb-tab
+          v-for="(scheduleDay, index) in scheduleList"
+          :heading="scheduleDay.header">
+        <text>{{scheduleDay.header}}</text>
+      </nb-tab>
+    </nb-tabs>
 
   </nb-container>
 </template>
 <script>
+import React from "react";
+import { ScrollableTab } from "native-base";
 
 export default {
   name: 'schedule',
@@ -34,12 +37,58 @@ export default {
     scheduleReady() {
       return true;
     },
+    scheduleList() {
+      return [{
+        header: 'Понедельник',
+        classes: [{
+          label: 'Матан',
+          office: '250 аудитория',
+          lecturer: 'к. ф. м. наук Здор А.Г.',
+          type: 1, // 1 семинар, 2 лекция
+          begin: '',
+          end: '',
+          notice: '',
+        }],
+      }, {
+        header: 'Вторник',
+        classes: [{
+
+        }],
+      }, {
+        header: 'Среда',
+        classes: [{
+
+        }],
+      }, {
+        header: 'Четверг',
+        classes: [{
+
+        }],
+      }, {
+        header: 'Пятница',
+        classes: [{
+
+        }],
+      }, {
+        header: 'Суббота',
+        classes: [{
+
+        }],
+      }, {
+        header: 'Воскресенье',
+        classes: [{
+
+        }],
+      }];
+    },
   },
-  methods: {},
+  methods: {
+    getScollableTabComp() {
+      return <ScrollableTab />;
+    },
+  },
   data() {
     return {
-      schedule: [{
-      }],
     };
   },
 };
